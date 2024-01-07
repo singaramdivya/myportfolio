dec8e1a42abf54caaa023a4a0a0d2ee55753e8e7
Divya Singaram <Divya@purplenowtech.com>
2024-01-07 01:50:50 -0600

    changes done in about file

--- src/components/About.js
+++ src/components/About.js
@@ -5,7 +5,8 @@ import './style.css'
 class About extends Component {
   render() {
     return (
-      <div className='about-section'>
+      <>
+        <div className='about-section'>
         <div className='about-content'>
           <h2>I am <span>Divya Singaram</span></h2>
           <p>Fresher With a passion for building innovative solutions and about taking risks to solve them by communicating and sharing experience and knowledge to become a Software Developer</p>
@@ -20,6 +21,7 @@ class About extends Component {
           <img className='about-img' src="https://img.freepik.com/premium-vector/portrait-elegant-business-woman-with-arms-crossed-isolated-vector-illustration_126609-502.jpg?w=740" alt="about-img"/>
         </div>
       </div>
+      </>
     );
   }
 }
